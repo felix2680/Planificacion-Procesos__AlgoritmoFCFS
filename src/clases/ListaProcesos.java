faa509eba3dc1d2268976b8202008a6845fe2bcc
felix <felix@FELIX2680>
2023-09-20 12:02:44 -0600

    se añadió la clase que contendrá la cola de procesos

---
+++ src/clases/ListaProcesos.java
@@ -0,0 +1,27 @@
+package clases;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Random;
+
+public class ListaProcesos {
+    Queue<Proceso> colaProcesos;
+    private char[] operaciones = {'+','-','*','/','%'};
+    
+    public ListaProcesos(int cantidadProcesos){
+        colaProcesos = new LinkedList<>();
+        llenarColaProcesos(cantidadProcesos);
+    }
+    
+    private void llenarColaProcesos(int cantidadProcesos){
+        for(int i = 1; i <= cantidadProcesos; i++){
+            Proceso p = new Proceso();
+            p.establecerID(i);
+            p.establecerDato1(new Random().nextInt(100)+1);
+            p.establecerDato2(new Random().nextInt(100)+1);
+            p.establecerOperacion(operaciones[new Random().nextInt(5)]);
+            p.establecerTiempoEstimado(new Random().nextInt(12)+7);
+            this.colaProcesos.offer(p);
+        }
+    }
+}
