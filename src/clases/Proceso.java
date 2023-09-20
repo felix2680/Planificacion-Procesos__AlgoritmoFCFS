e5139ceded42a0e0c43ff189824cdb78000831c4
felix <felix@Felix2680>
2023-09-19 23:43:20 -0600

    Creacion del nuevo proyecto

---
+++ src/clases/Proceso.java
@@ -0,0 +1,89 @@
+package clases;
+
+public class Proceso {
+    private int ID;
+    private char operacion;
+    private double dato1;
+    private double dato2;
+    private int tiempoEstimado;
+    private double resultado;
+    private boolean error;
+    private boolean interrumpido;
+    
+    private int tiempoRestante;
+    public  Proceso(){
+        this.ID = 0;
+        this.operacion = ' ';
+        this.dato1 = 0;
+        this.dato2 = 0;
+        this.tiempoEstimado = 0;
+        this.resultado = 0;
+        this.error = false;
+        this.tiempoRestante = 0;
+    }
+    public void establecerID(int ID){
+        this.ID = ID;
+    }
+    public void establecerOperacion(char operacion){
+        this.operacion = operacion;
+    }
+    public void establecerDato1(double dato1){
+        this.dato1 = dato1;
+    }
+    public void establecerDato2(double dato2){
+        this.dato2 = dato2;
+    }
+    public void establecerTiempoEstimado(int tiempoEstimado){
+        this.tiempoEstimado = tiempoEstimado;
+    }
+    public void establecerError(boolean error){
+        this.error = error;
+    }
+    
+    public void establecerInterrumpido(boolean interrumpido){
+        this.interrumpido = interrumpido;
+    }
+    public void establecerTiempoRestante(int tiempoRestante){
+        this.tiempoRestante = tiempoRestante;
+    }
+
+    public int obtenerID(){
+        return ID;
+    }
+    public char obtenerOperacion(){
+        return operacion;
+    }
+    public double obtenerDato1(){
+        return dato1;
+    } 
+    public double obtenerDato2(){
+        return dato2;
+    }
+    public int obtenerTiempoEstimado(){
+        return tiempoEstimado;
+    }
+    public double obtenerResultado(){
+        return realizarOperacon();
+    }
+    public boolean hayError(){
+        return error;
+    }
+    public int obtenerTiempoRestante(){
+        return this.tiempoRestante;
+    }
+    
+    public boolean obtenerInterrumpido(){
+        return this.interrumpido;
+    }
+    private double realizarOperacon(){        
+        switch(this.obtenerOperacion()){   
+            case '+' -> resultado = dato1 + dato2;
+            case '-' -> resultado = dato1 - dato2;
+            case '*' -> resultado = dato1 * dato2;
+            case '/' -> resultado = dato1 / dato2;
+            case '%' -> resultado = dato1 % dato2;
+            case '^' -> resultado = Math.pow(dato1,dato2);
+        }       
+        return resultado;
+    }
+}
---
+++ src/clases/ventanas/CapturarProcesos.java
@@ -0,0 +1,130 @@
+package clases.ventanas;
+
+
+import clases.Proceso;
+import java.awt.Color;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class CapturarProcesos extends javax.swing.JFrame {
+
+    private int cantidadProcesos;
+
+    List<Proceso> listaProceso;
+    Proceso p;
+    //EjecutarProcesos e;
+    char[] operaciones = {'+','-','*','/','%'};
+
+    public CapturarProcesos() {
+        initComponents();
+        this.setLocationRelativeTo(null);
+        this.getContentPane().setBackground(Color.decode("#71C5E8"));
+
+    //    e = new EjecutarProcesos();
+        listaProceso = new ArrayList<>();
+    }
+
+    public void llenarDatos() {
+        for (int i = 1; i <= cantidadProcesos; i++) {
+            p = new Proceso();
+            p.establecerID(i);
+            p.establecerDato1(new Random().nextInt(100)+1);
+            p.establecerDato2(new Random().nextInt(100)+1);
+            p.establecerOperacion(operaciones[new Random().nextInt(5)]);
+            p.establecerTiempoEstimado(new Random().nextInt(12)+7);
+            listaProceso.add(p);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        lblProcesos = new javax.swing.JLabel();
+        spCantidadProcesos = new javax.swing.JSpinner();
+        btnAceptar = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        lblProcesos.setFont(new java.awt.Font("Segoe UI", 3, 24)); // NOI18N
+        lblProcesos.setText("Ingresa la cantidad de procesos");
+
+        spCantidadProcesos.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        spCantidadProcesos.setModel(new javax.swing.SpinnerNumberModel(1, 1, null, 1));
+
+        btnAceptar.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        btnAceptar.setText("Aceptar");
+        btnAceptar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnAceptarActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(23, 23, 23)
+                        .addComponent(lblProcesos))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(151, 151, 151)
+                        .addComponent(spCantidadProcesos, javax.swing.GroupLayout.PREFERRED_SIZE, 97, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(132, 132, 132)
+                        .addComponent(btnAceptar, javax.swing.GroupLayout.PREFERRED_SIZE, 138, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(40, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(42, 42, 42)
+                .addComponent(lblProcesos)
+                .addGap(28, 28, 28)
+                .addComponent(spCantidadProcesos, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(btnAceptar)
+                .addContainerGap(83, Short.MAX_VALUE))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void btnAceptarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAceptarActionPerformed
+        cantidadProcesos = (int) spCantidadProcesos.getValue();
+        llenarDatos();
+        //e.inicializarPrograma(listaProceso);
+        //e.setVisible(true);
+        this.setVisible(false);
+    }//GEN-LAST:event_btnAceptarActionPerformed
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(CapturarProcesos.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+
+        java.awt.EventQueue.invokeLater(() -> {
+            new CapturarProcesos().setVisible(true);
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnAceptar;
+    private javax.swing.JLabel lblProcesos;
+    private javax.swing.JSpinner spCantidadProcesos;
+    // End of variables declaration//GEN-END:variables
+}
